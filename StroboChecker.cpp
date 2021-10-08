e1676e513ce5972ea652b3b23692c99a2be40713
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-08 16:51:23 -0500

    first sample run coded

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -1,10 +1,26 @@
-/*
- * StroboChecker.cpp
- *
- *  Created on: Oct 8, 2021
- *      Author: mstep33
- */
+/**
+* This program will determine if a number is a strobogrammatic number and is viewed the same upside down as it is right side up. <br>
+* @author Mitchell Stephany
+* <pre>
+* DATE: November 8, 2021
+* File: StroboChecker.cpp
+* Course: csc1253 Section x <br>
+* Programming Project #: 2 <br
+* Instructor: Dr. Duncan
+* </pre>
+*/
 
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	int n;
+	if(n < 0){
+		cout<<"You must enter a nonnegative integer.";
+	}
+	else
+		cout<<"That could work. I haven't coded that yet though.";
+}
 
 
