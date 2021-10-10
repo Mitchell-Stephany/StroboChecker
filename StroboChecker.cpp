83a345773f2d35403bee5c4109c295173402ce84
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-09 20:36:37 -0500

    edited

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -11,19 +11,60 @@
 */
 
 #include <iostream>
-
+#include <sstream>
 using namespace std;
 
-int main(){
-	int n;
+int main()
+{
+	long number;
+	int digCount = 0;
+	int reversedNumber = 0;
 
 	cout<< "Please enter a number -> ";
-	cin>> n;
-	if(n < 0){
+	cin>> number;
+
+	if(number < 0)
+	{
 		cout<< "You must enter a nonnegative integer.";
 	}
 	else
-		cout<< "That could work. I haven't coded that yet though.";
-}
+	{
+	while(number > 0)
+	{
+		number /= 10;
+		digCount++;
+	}
+	}
+	cout<< "The number contains " << digCount << " digits. ";
+
+
+
+	if(digCount % 2 == 0)
+	{
+		cout << "The number is not a strobogrammatical number.";
+	}
+	else
+	{
+		cout << "The number could be a strobogrammatical number";
+	}
+	while (number % 2 != 0)
+		{
+			reversedNumber *= 10;
+			int digLast = number % 10;
+			reversedNumber += digLast;
+			number /= 10;
+			}
+
+		cout<<" Reversed number is: " << reversedNumber;
+	return 0;
+	}
+
+
+
+
+
+
+
+
 
 
