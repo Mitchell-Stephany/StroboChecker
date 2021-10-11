8a6047e18aacdc1be7a22cfef73ccc2dc981f507
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-10 21:02:53 -0500

    Some updated digit counter and such

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -11,52 +11,47 @@
 */
 
 #include <iostream>
-#include <sstream>
 using namespace std;
 
-int main()
-{
-	long number;
-	int digCount = 0;
-	int reversedNumber = 0;
+	int main(){
+		int i = 0;
+		int number;
+		string intString;
 
-	cout<< "Please enter a number -> ";
-	cin>> number;
+		cout << "Enter a nonnegative integer -> ";
+		cin >> number;
 
-	if(number < 0)
-	{
-		cout<< "You must enter a nonnegative integer.";
-	}
-	else
-	{
-	while(number > 0)
-	{
-		number /= 10;
-		digCount++;
-	}
-	}
-	cout<< "The number contains " << digCount << " digits. ";
-
-
-
-	if(digCount % 2 == 0)
-	{
-		cout << "The number is not a strobogrammatical number.";
-	}
-	else
-	{
-		cout << "The number could be a strobogrammatical number";
-	}
-	while (number % 2 != 0)
+		if(number < 0)
 		{
-			reversedNumber *= 10;
-			int digLast = number % 10;
-			reversedNumber += digLast;
-			number /= 10;
-			}
+			cout << "You must enter a nonnegative integer.";
+		}
+		else if(number == 0 || number == 1 || number == 8)
+		{
+			cout << number << " is a strobogrammatical number." << endl;
+		}
 
-		cout<<" Reversed number is: " << reversedNumber;
-	return 0;
+		intString = to_string(number);
+		char digit = intString.at(i);
+		int numDigits = intString.length();
+		cout << "The number of digits is " << numDigits << "." << endl;
+		cout << "The first digit is " << digit << "." << endl;
+
+		intString[0] = 0;
+		intString[1] = 1;
+		intString[6] = 9;
+		intString[8] = 8;
+		intString[9] = 6;
+
+		for (int i = 0; i < numDigits / 2; i++)
+		{
+			int left = intString.at(i);
+			int right = (digit - 1 - i);
+
+			if(left != right)
+			{
+				cout << "Error.";
+			}
+		}
 	}
 
 
