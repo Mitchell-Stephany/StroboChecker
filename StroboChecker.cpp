96dba0a5b84168af6d1b5b668743d2cab4e7722c
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-15 12:10:31 -0500

    completely wiped and started from scratch

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -12,67 +12,33 @@
 
 #include <iostream>
 using namespace std;
+int main(){
+	long number; //this is a long because the user could input long numbers
+	cout << "Please enter a nonnegative integer -> ";
+	cin >> number;
+	if(number < 0)
+	{
+		cout << "You must enter a nonnegative integer.";
+	}
+	string numberString = to_string(number);
+	int numDigits = numberString.length();
 
-	int main(){
-		int number;
-		string intString;
+	char digitFirst = numberString.at(0); //this line grabs the first digit from the entered integer
+	char digitMiddle = numberString.at((numDigits - 1) / 2); //grabs middle digit
+	char digitFinal = numberString.at(numDigits -1); // grabs final digit
 
-		cout << "Enter a nonnegative integer -> ";
-		cin >> number;
-
-		if(number < 0)
-		{
-			cout << "You must enter a nonnegative integer.";
-		}
-		else if(number == 0 || number == 1 || number == 8)
-		{
-			cout << number << " is a strobogrammatical number." << endl;
-		}
-
-		intString = to_string(number);
-//		char digit = intString.at(0);
-		int numDigits = intString.length();
-		int digitEnd = number % 10;
-
-		cout << "The number of digits is " << numDigits << "." << endl;
-//		cout << "The first digit is " << digit << "." << endl;
-		cout << "The final digit is " << digitEnd << "." << endl;
-
-		while(number >= 10)
-				{
-					number = number / 10;
-				}
-					cout << "The first digit is " << number << "." << endl;
-		if(digitEnd == number)
-		{
-			cout << "Loser.";
-		}
-
-//		intString[0] = 0;
-//		intString[1] = 1;
-//		intString[6] = 9;
-//		intString[8] = 8;
-//		intString[9] = 6;
-
-//		for (int i = 0; i < numDigits / 2; i++)
-//		{
-//			int left = intString.at(i);
-//			int right = (digitEnd);
-
-//			if(left != right)
-//			{
-//				cout << "Error.";
-//			}
-//		}
-		return 0;
+	if(number == 0 || number == 1 || number == 8 || number == 11 || number == 69 || number == 88 || number == 96)
+	{
+		cout << number << " is a strobogrammatical number." << endl;
+	}
+	else if(numDigits %2 != 0 && (digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8)) //written to see if the number of digits in an integer is odd
+	{
+		cout << "This could not work." << endl;
 	}
 
 
+	cout << digitFirst << endl;
+	cout << digitMiddle << endl;
+	cout << digitFinal << endl;
 
-
-
-
-
-
-
-
+}
