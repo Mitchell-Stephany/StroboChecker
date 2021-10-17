14a950a6b035b70d89af4b818e627dbee814aeaa
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-17 17:48:02 -0500

    finished code

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -12,68 +12,61 @@
 
 #include <iostream>
 using namespace std;
-int main(){
+long revNum(long num)
+{
+	int remainder, reversedNumber;
+	while(num != 0)
+	{
+        remainder = num % 10;
+        reversedNumber = reversedNumber*10 + remainder;
+        num /= 10;
+    }
+	return reversedNumber;
+}
+int main()
+{
 	long number;
-//	int reversedNumber = 0;
-//	int i; // nested for loop with if statement
 	cout << "Please enter a nonnegative integer -> ";
 	cin >> number;
-
-	string numberString = to_string(number);
-	int numDigits = numberString.length();
-	char digitFirst = numberString.at(0); //this line grabs the first digit from the entered integer
-	char digitMiddle = numberString.at((numDigits - 1) / 2); //grabs middle digit
-	char digitFinal = numberString.at(numDigits - 1); // grabs final digit
-
-//	string rNumberString = to_string(reversedNumber);
-//	int numRDigits = rNumberString.length();
-//	char digitRMiddle = rNumberString.at((numRDigits - 1) / 2);
-//	char digitRFirst = rNumberString.at(0);
-
-	cout << numDigits << endl;
-	cout << digitMiddle << endl;
-
+	long reverseNum = revNum(number);
+	string numStr = to_string(number);
+	int numDigits = numStr.length();
+	char midDig = numStr.at(numDigits / 2);
+	string rNumberString = to_string(reverseNum);
 	if(number < 0)
 	{
 		cout << "Error: You must enter a nonnegative integer." << endl;
 	}
-
-	if((numDigits % 2 != 0) && (digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8))
+	else if(number < 10)
 	{
-//		if(digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8)
-//		{
-			cout << number << " is not a strobogrammatical number." << endl;		}
-//		}
+		if(number == 0 || number == 1 || number == 8)
+		{
+			cout << number << " is a strobogrammatical number." << endl;
+		}
+		else
+			cout << number << " is not a strobogrammatical number." << endl;
+	}
+	else if(numDigits % 2 == 1 && !(midDig == '0' || midDig == '1' || midDig == '8'))
+	{
+		cout << number << " is not a strobogrammatical number." << endl;
+	}
 	else
 	{
-		cout << number << " could be strobogrammatical." << endl;
-	}
-
-
-// what is
-	if(number == 0 || number == 1 || number == 8 || number == 11 || number == 88) //single and double digit #s
-	{
+		for(int i = 0; i < numDigits / 2; i++)
+		{
+			cout << numStr.at(i) << " <-> " << rNumberString.at(i) << endl;
+			if(!((numStr.at(i) == rNumberString.at(i)) || (numStr.at(i) == '6' && rNumberString.at(i) == '9') || (numStr.at(i) == '9' && rNumberString.at(i) == '6')) || (numStr.at(i) == '6' && rNumberString.at(i) == '6'))
+			{
+				if((numStr.at(i) == '2' || numStr.at(i) == '4' || numStr.at(i) == '5' || numStr.at(i) == '6' || numStr.at(i) == '7') || numStr.at(i) == '9' || !(midDig == '0' || midDig == '1' || midDig == '8'))
+				{
+					cout << number << " is not a strobogrammatical number.";
+					return 0;
+				}
+				cout << number << " is not a strobogrammatical number.";
+				return 0;
+			}
+		}
 		cout << number << " is a strobogrammatical number." << endl;
-	}
-	if(digitFirst == digitFinal)
-	{
-	//		for(i = 1; i <= numDigits / 2; i++)
-	//		{
-	//			while(number != 0)
-	//				{
-	//					reversedNumber *= 10;
-	//					reversedNumber += number % 10;
-	//					number /= 10;
-	//				}
-
-				cout << digitFirst << " <-> " << digitFinal << endl;
-	//			cout << numberString.at(i) << " <-> " << rNumberString.at(i) << endl;
-
-	//		}
-	}
-	else
-	{
-		cout << "Hello." << endl;
 	}
 	return 0;
 }
