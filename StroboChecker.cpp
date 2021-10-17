2143cdf5ba7f4e77ee4417f492f6d03cdab1f8c7
Mitchell <35747185+Mitchell-Stephany@users.noreply.github.com>
2021-10-17 00:13:45 -0500

    bad

--- StroboChecker.cpp
+++ StroboChecker.cpp
@@ -14,8 +14,8 @@
 using namespace std;
 int main(){
 	long number;
-	int reversedNumber = 0;
-	int i; // nested for loop with if statement
+//	int reversedNumber = 0;
+//	int i; // nested for loop with if statement
 	cout << "Please enter a nonnegative integer -> ";
 	cin >> number;
 
@@ -25,47 +25,56 @@ int main(){
 	char digitMiddle = numberString.at((numDigits - 1) / 2); //grabs middle digit
 	char digitFinal = numberString.at(numDigits - 1); // grabs final digit
 
-	string rNumberString = to_string(reversedNumber);
-	int numRDigits = rNumberString.length();
-	char digitRMiddle = rNumberString.at((numRDigits) / 2);
+//	string rNumberString = to_string(reversedNumber);
+//	int numRDigits = rNumberString.length();
+//	char digitRMiddle = rNumberString.at((numRDigits - 1) / 2);
+//	char digitRFirst = rNumberString.at(0);
 
-//	while(number != 0)
-//	{
-//		reversedNumber *= 10;
-//		reversedNumber += number % 10;
-//		number /= 10;
-//	}
+	cout << numDigits << endl;
+	cout << digitMiddle << endl;
 
 	if(number < 0)
 	{
 		cout << "Error: You must enter a nonnegative integer." << endl;
 	}
-	else if(number == 0 || number == 1 || number == 8 || number == 11 || number == 88) //single and double digit #s
+
+	if((numDigits % 2 != 0) && (digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8))
+	{
+//		if(digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8)
+//		{
+			cout << number << " is not a strobogrammatical number." << endl;		}
+//		}
+	else
+	{
+		cout << number << " could be strobogrammatical." << endl;
+	}
+
+
+// what is
+	if(number == 0 || number == 1 || number == 8 || number == 11 || number == 88) //single and double digit #s
 	{
 		cout << number << " is a strobogrammatical number." << endl;
 	}
-	else if(numDigits %2 != 0 && (digitMiddle != 0 || digitMiddle != 1 || digitMiddle != 8)) //written to see if the number of digits in an integer is odd
+	if(digitFirst == digitFinal)
 	{
-		cout << number << " is not a strobogrammatical number." << endl;
-	}
-	else if(digitFirst == digitFinal)
-	{
-			for(i = 1; i <= numDigits / 2; i++)
-			{
-				cout << digitFirst << " <-> " << digitFinal << endl;
-				cout << numberString.at(i) << " <-> " << rNumberString.at(i) << endl;
+	//		for(i = 1; i <= numDigits / 2; i++)
+	//		{
+	//			while(number != 0)
+	//				{
+	//					reversedNumber *= 10;
+	//					reversedNumber += number % 10;
+	//					number /= 10;
+	//				}
 
-			}
+				cout << digitFirst << " <-> " << digitFinal << endl;
+	//			cout << numberString.at(i) << " <-> " << rNumberString.at(i) << endl;
+
+	//		}
 	}
 	else
 	{
 		cout << "Hello." << endl;
 	}
-
-	cout << digitRMiddle << endl;
-	cout << digitMiddle << endl;
-	cout << reversedNumber << endl;
-
 	return 0;
 }
 
